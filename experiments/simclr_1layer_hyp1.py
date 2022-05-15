f2f569246991231f46b8b552ec9bdead7ea0caf4
Viet Anh Khoa Tran <khoa.tran@rwth-aachen.de>
2022-05-15 16:44:40 +0200

    wip

--- experiments/simclr_1layer_hyp1.py
+++ experiments/simclr_1layer_hyp1.py
@@ -66,8 +66,9 @@ class Experiment(BaseExperiment):
             is_abstract=True,
             python_search_path=["/p/project/jinm60/users/tran4/biasadapt_git"],
             env={"WANDB_MODE": "offline",
-                 "WANDB_DIR": "/p/project/jinm60/users/tran4/out_biasadapt/wandb",
-                 "WANDB_RESUME": "auto"},
+                 "WANDB_CACHE_DIR": "/p/scratch/jinm60/tran4/wandb",
+                 "WANDB_RESUME": "auto",
+                 "WANDB_GROUP": "emnist_simclr_pretrain"},
             parameters={
                 "data_path": "/p/project/jinm60/users/tran4/datasets",
                 "wandb_project": "biasadapt",
