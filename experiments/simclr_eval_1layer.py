778cdf3ce69c249593eb801aa3d57813256f48f0
Viet Anh Khoa Tran <khoa.tran@rwth-aachen.de>
2022-05-22 17:40:45 +0200

    .

--- experiments/simclr_eval_1layer.py
+++ experiments/simclr_eval_1layer.py
@@ -34,7 +34,6 @@ class Experiment(BaseExperiment):
                     project="jinm60",
                     cores=128,
                     memory="500G",
-                    interface="ib0",
                     scheduler_options={'interface': 'ib0'},
                     local_directory="/p/scratch/jinm60/tran4/dask",
                     log_directory=(self.path / "slurm-logs").as_posix(),
