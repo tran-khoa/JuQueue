0d5f044ec472481158715090103b58b95a55c0d9
Viet Anh Khoa Tran <khoa.tran@rwth-aachen.de>
2022-06-10 15:28:44 +0200

    Fixed unique_hash in DefUtils

--- juqueue/definitions/utils.py
+++ juqueue/definitions/utils.py
@@ -3,9 +3,9 @@ import hashlib
 
 def unique_hash(string: str, length: int = 24) -> str:
     """
-    Generates an alphanumeric hash for the given string of the given length (maximum: 56)
+    Generates an alphanumeric hash for the given string of the given length (maximum: 128)
     """
-    if not 0 < length <= 56:
+    if not 0 < length <= 128:
         raise ValueError("Length must be an integer in the interval (0, 56].")
 
-    return hashlib.sha224(string.encode("utf8"))[:length]
+    return hashlib.sha512(string.encode("utf8")).hexdigest()[:length]
